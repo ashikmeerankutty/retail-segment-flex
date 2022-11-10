36847a6dce22a1dbab7a421b715024366b79a6dc
ssepactwilio <ssepac@twilio.com>
2022-11-10 10:43:20 -0700

    finished home page and product view layouts

--- website/components/SizeSelection/SizeGrid.tsx
+++ website/components/SizeSelection/SizeGrid.tsx
@@ -1,4 +1,4 @@
-import { Grid, Stack, Text } from "@twilio-paste/core";
+import { Flex, Grid, Stack, Text } from "@twilio-paste/core";
 import { useMemo } from "react";
 import { IAvailbleSize } from "../../Global.types";
 import SizeButton from "./SizeButton";
@@ -8,35 +8,38 @@ export interface ISizeGridProps {
 }
 
 const SizeGrid = ({ sizes }: ISizeGridProps) => {
+  const BUTTONS_PER_ROW = 5;
   const buttonRows = useMemo(() => {
     if (sizes.length > 0) {
       const sizesCopy = [...sizes];
       const output = [];
-      while (sizesCopy.length > 0) output.push(sizesCopy.splice(0, 5));
+      while (sizesCopy.length > 0) output.push(sizesCopy.splice(0, BUTTONS_PER_ROW));
 
-      return (<Stack orientation="vertical" spacing="space50">
-        {output.map((splice, index) => (
-          <Stack
-            key={`button-group-${index}`}
-            orientation="horizontal"
-            spacing="space30"
-          >
-            {splice.map(({ size, available }: IAvailbleSize) => (
-              <SizeButton
-                key={`sizebutton-${size}`}
-                value={size}
-                disabled={!available}
-              />
-            ))}
-          </Stack>
-        ))}
-      </Stack>)
+      return (
+        <Stack orientation="vertical" spacing="space50">
+          {output.map((splice, index) => (
+            <Stack
+              key={`button-group-${index}`}
+              orientation="horizontal"
+              spacing="space30"
+            >
+              {splice.map(({ size, available }: IAvailbleSize) => (
+                <SizeButton
+                  key={`sizebutton-${size}`}
+                  value={size}
+                  disabled={!available}
+                />
+              ))}
+            </Stack>
+          ))}
+        </Stack>
+      );
     }
   }, [sizes]);
 
   return (
-    <Stack orientation="vertical" spacing="space0">
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <div style={{flexDirection: 'column', display: 'inline-flex'}}>
+      <div style={{ display: "flex", justifyContent: "space-between", paddingBottom: '10px'}}>
         <Text as="p" fontWeight="fontWeightBold">
           Select Size
         </Text>
@@ -45,7 +48,7 @@ const SizeGrid = ({ sizes }: ISizeGridProps) => {
         </Text>
       </div>
       {buttonRows}
-    </Stack>
+    </div>
   );
 };
 
